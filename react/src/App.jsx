5c74d39362053246c9c18554230851b6bb31a65a
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2025-01-26 10:50:58 +0530

    Add files via upload

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 import './App.css'
-import {Box, Button, TextField, Alert} from "@mui/material";
-// import { styled } from "@mui/system";
+import {Box, Button, TextField, Alert, Typography} from "@mui/material";
+import { styled } from "@mui/system";
 
-// const FormContainer = styled(Box) ({
-//   border: "1px solid red"
-// });
+const FormContainer = styled(Box) ({
+  border: "1px solid #ddd",
+  maxWidth: 500,
+  margin: "50px auto",
+  padding: "20px",
+  boxShadow: "3px 3px 10px rgba(0,0,0,0.1)"
+});
 
-// const FileInput = styled("input")({
-//   display: "none"
-// });
+const FileInput = styled("input")({
+  display: "none"
+});
 
 function App() {
 
@@ -18,18 +22,57 @@ function App() {
     email: "",
     password: "",
     confirmPassword: "",
-    dob: ""
+    dob: "",
+    file: null
   });
 
   const [errors, setErrors] = useState({});
+  const [fileError, setFileError] = useState("");
   const [success, setSuccess] = useState(false);
 
   const errMsg = {};
 
   const validateForm = () => {
+
+    // Validate Username - length should be greater than 4
     if(!formData.username || formData.username.length <= 4) {
       errMsg.username = "Username must be greater than 5 characters";
     }
+
+    // Validate Email - should match the email pattern
+    const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!formData.email || !emailRegEx.test(formData.email)) {
+      errMsg.email = "Invalid Email Address";
+    }
+
+    // TODO
+    // Validate Password - test for password strength using regex
+    // Confirm Password - test for password and confirm password should match
+
+    // Validate DOB - Age should be greater than 18
+    const currentYear = new Date().getFullYear();
+    const birthYear = new Date(formData.dob).getFullYear();
+    const age = currentYear - birthYear;
+    if(!formData.dob || age < 18) {
+      errMsg.dob = "Age must be at least 18 years";
+    }
+
+    // Validate file size
+    if(formData.file) {
+      const fileSizeKb = formData.file.size / 1024;
+      if(fileSizeKb > 5) {
+        setFileError("File size must be less than 5KB")
+      }
+      else {
+        setFileError("");
+      }
+    } else {
+      setFileError("Please upload file");
+    }
+
+
+    setErrors(errMsg);
+    return Object.keys(errMsg).length === 0;
   }
 
   const changeHandler = (e) => {
@@ -37,16 +80,31 @@ function App() {
     setFormData({...formData, [name]:value});
   }
 
+  const handleFileChange = (e) => {
+    setFormData({...formData, file: e.target.files[0]});
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(validateForm()) {
+      setSuccess(true);
+    }
+  }
+
   return (
     <>
-      {/* <FormContainer> */}
-        <form>
+       <FormContainer>
+        <h1>Registration Form</h1>
+        <form onSubmit={handleSubmit}>
           <TextField 
             label="Username"
             name="username"
             fullWidth
             value={formData.username}
             onChange={changeHandler}
+            error={!errors.username}
+            helperText={errors.username}
           />
 
           <TextField 
@@ -54,6 +112,10 @@ function App() {
             name="email"
             fullWidth
             margin='normal'
+            value={formData.email}
+            onChange={changeHandler}
+            error={!errors.email}
+            helperText={errors.email}
           />
 
           <TextField 
@@ -79,13 +141,27 @@ function App() {
             fullWidth
             margin='normal'
             InputLabelProps={{shrink:true}}
+            onChange={changeHandler}
+            error={!errors.dob}
+            helperText={errors.dob}
           />
 
-          {/* <label htmlFor='file-upload'
+          <label htmlFor='file-upload'>
             <FileInput 
               id="file-upload"
+              type='file'
+              onChange={handleFileChange}
             />
-          /> */}
+            <Button variant='outlined' component="span" fullWidth>
+              Upload File
+            </Button>
+          </label>   
+          {fileError && (
+            <Typography color='error' mt={1}>
+              {fileError}
+            </Typography>
+          )}
+
 
           <Button
             type='submit'
@@ -96,7 +172,7 @@ function App() {
 
           
         </form>
-      {/* </FormContainer> */}
+      </FormContainer>
     </>
   )
 }
