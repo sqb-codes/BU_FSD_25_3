9a246e455bd0330d4010abae1655b697026cb0b8
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2025-04-07 15:22:39 +0530

    Add files via upload

--- chat-app/App.js
+++ chat-app/App.js
@@ -1,12 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SignIn from './screens/SignIn';
+// npm install @react-navigation/native @react-navigation/stack
+// npm install @react-navigation/material-top-tabs
+import {createStackNavigator} from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs"
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import { ChatHome } from './screens/ChatHome';
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+  const Tab = createMaterialTopTabNavigator();
+
+  const [currUser, setCurrUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if(user) {
+        setCurrUser(user)
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <SignIn/>
-    </View>
+    // <View style={styles.container}>
+    //   <SignIn/>
+    // </View>
+
+    <NavigationContainer style={styles.container}>
+      {
+        !currUser ? (
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="SignIn" component={SignIn}/>
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={ChatHome}/>
+          </Stack.Navigator>
+        )
+      }
+    </NavigationContainer>
   );
 }
 
@@ -15,6 +55,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
